d1556b64248bfb5700a00acbbb0a86a5879be33b
Himanshu Arora <himanshuarora323@gmail.com>
2018-08-29 14:30:05 +0530

    Added port for Heroku

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express=require("express");
 const hbs=require("hbs");
 const fs =require("fs")
 
+const port=process.env.PORT || 3000;
+
 let app=express();
 
 hbs.registerPartials(__dirname+"/views/partials")
@@ -71,7 +73,7 @@ app.get("/bad",(req,resp)=>{
     })
 })
 
-app.listen(3000,()=>{
+app.listen(port,()=>{
 
-    console.log("Server is running on port 3000")
+    console.log(`Server is running on port ${port}`)
 });
